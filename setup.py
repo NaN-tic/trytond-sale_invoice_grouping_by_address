6bb7353b105fbe1fe7d179bc054a23bc8cd87c0a
jmartin <jmartin@zikzakmedia.com>
2013-11-27 13:26:58 +0100

    Several fixes.

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ tests_require = ['proteus >= %s.%s, < %s.%s' %
 
 setup(name='%s_%s' % (PREFIX, MODULE),
     version=info.get('version', '0.0.1'),
-    description='Tryton module to store customer reference of sales.',
+    description='Tryton module to group sale invoices by shipment address.',
     author='Zikzakmedia SL',
     author_email='zikzak@zikzakmedia.com',
     url='http://www.zikzakmedia.com',
