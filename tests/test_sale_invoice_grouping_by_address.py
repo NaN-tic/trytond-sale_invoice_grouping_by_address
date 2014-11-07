6af4cf7a78805b45cd73e4249f6f09472e7f0205
jmartin <jmartin@zikzakmedia.com>
2014-11-07 14:38:16 +0100

    Cleanup tests

--- tests/test_sale_invoice_grouping_by_address.py
+++ tests/test_sale_invoice_grouping_by_address.py
@@ -1,40 +1,26 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
-#This file is part of the sale_invoice_grouping_by_address module for Tryton.
-#The COPYRIGHT file at the top level of this repository contains
-#the full copyright notices and license terms.
-
-import sys
-import os
-DIR = os.path.abspath(os.path.normpath(os.path.join(__file__,
-    '..', '..', '..', '..', '..', 'trytond')))
-if os.path.isdir(DIR):
-    sys.path.insert(0, os.path.dirname(DIR))
-
+# This file is part of the sale_invoice_grouping_by_address module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
 from trytond.tests.test_tryton import test_view, test_depends
 
 
 class SaleInvoiceGroupingByAddressTestCase(unittest.TestCase):
-    '''
-    Test Sale Invoice Grouping By Address module.
-    '''
+    'Test Sale Invoice Grouping By Address module'
 
     def setUp(self):
         trytond.tests.test_tryton.install_module(
             'sale_invoice_grouping_by_address')
 
     def test0005views(self):
-        '''
-        Test views.
-        '''
+        'Test views'
         test_view('sale_invoice_grouping_by_address')
 
     def test0006depends(self):
-        '''
-        Test depends.
-        '''
+        'Test depends'
         test_depends()
 
 
