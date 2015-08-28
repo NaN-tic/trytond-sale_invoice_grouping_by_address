a5478804ad38ec78ef25beff091f8243f5fcc153
resteve <resteve@zikzakmedia.com>
2015-08-28 12:25:59 +0200

    New API for on_change

--- invoice.py
+++ invoice.py
@@ -17,16 +17,15 @@ class Invoice():
             },
         depends=['party', 'state'])
 
+    @fields.depends('party')
     def on_change_party(self):
-        res = super(Invoice, self).on_change_party()
-        res['shipment_address'] = None
-        res['shipment_address.rec_name'] = None
+        super(Invoice, self).on_change_party()
+
+        self.shipment_address = None
         if self.party:
             delivery_address = self.party.address_get(type='delivery')
             if delivery_address:
-                res['shipment_address'] = delivery_address.id
-                res['shipment_address.rec_name'] = delivery_address.rec_name
-        return res
+                self.shipment_address = delivery_address
 
     def _credit(self):
         res = super(Invoice, self)._credit()
