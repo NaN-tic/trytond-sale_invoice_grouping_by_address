95c10a1a5a1a7d163f0b899f5181ef08bae18eee
resteve <resteve@zikzakmedia.com>
2015-12-01 11:19:15 +0100

    Tests

--- tests/test_sale_invoice_grouping_by_address.py
+++ tests/test_sale_invoice_grouping_by_address.py
@@ -1,27 +1,14 @@
-#!/usr/bin/env python
-# -*- coding: UTF-8 -*-
 # This file is part of the sale_invoice_grouping_by_address module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains
-# the full copyright notices and license terms.
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class SaleInvoiceGroupingByAddressTestCase(unittest.TestCase):
+class SaleInvoiceGroupingByAddressTestCase(ModuleTestCase):
     'Test Sale Invoice Grouping By Address module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module(
-            'sale_invoice_grouping_by_address')
-
-    def test0005views(self):
-        'Test views'
-        test_view('sale_invoice_grouping_by_address')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'sale_invoice_grouping_by_address'
 
 
 def suite():
@@ -29,6 +16,3 @@ def suite():
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
         SaleInvoiceGroupingByAddressTestCase))
     return suite
-
-if __name__ == '__main__':
-    unittest.TextTestRunner(verbosity=2).run(suite())
